6e28edd7d924ef073ccb17b4ab3721af83242140
Florian Rhiem <florian.rhiem@gmail.com>
2021-09-09 17:13:31 +0200

    Update package version to 2.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 
 For information on changes in GLFW itself, see the [GLFW version history](https://www.glfw.org/changelog.html).
 
+## [2.2.0] - 2021-09-09
+- Added Linux wheels for aarch64
+- Updated to GLFW 3.3.4
+
 ## [2.1.0] - 2021-02-28
 - Updated to GLFW 3.3.3
 
--- glfw/__init__.py
+++ glfw/__init__.py
@@ -9,7 +9,7 @@ from __future__ import unicode_literals
 __author__ = 'Florian Rhiem (florian.rhiem@gmail.com)'
 __copyright__ = 'Copyright (c) 2013-2021 Florian Rhiem'
 __license__ = 'MIT'
-__version__ = '2.1.0'
+__version__ = '2.2.0'
 
 # By default, GLFW errors will be handled by a pre-defined error callback.
 # Depending on the value of ERROR_REPORTING, this callback will:
